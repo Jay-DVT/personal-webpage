6a4e6874504b619eb26e9f66a56602c05db9cd52
Jay-DVT <diegovt.arbt@gmail.com>
2022-09-25 12:32:43 -0600

    Initial settings finalize

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Diego Villanueva</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>test</h1>
+    </div>
+  )
+}
+
+export default Home
