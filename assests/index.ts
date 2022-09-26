92493b14e13696dbb23fcc701e5d3402b8f0d42e
Jay-DVT <diegovt.arbt@gmail.com>
2022-09-25 19:42:15 -0600

    Create index for project, experience and socials

---
+++ assests/index.ts
@@ -0,0 +1,13 @@
+import css3 from './css3.svg'
+import instagram from './instagram.svg'
+import github from './github.svg'
+import linkedin from './linkedin.svg'
+import mail from './mail.svg'
+
+export {
+    css3,
+    instagram,
+    github,
+    linkedin,
+    mail,
+}
---
+++ constants/index.ts
@@ -0,0 +1,93 @@
+import { css3, mail, linkedin, github, instagram } from "../assests";
+
+export const experiences = [
+    {
+      id: "experience-1",
+      title: "Somethin Engineer",
+      Location: "Chihuahua, Mexico",
+      company: "bar",
+      achievements: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque.", 
+        "Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis",
+        "Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque"
+      ]  
+    },
+    {
+      id: "experience-2",
+      title: "Somethin ENgineer Secured",
+      Location: "Chihuahua, Mexico",
+      company: "bar",
+      achievements: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque.", 
+        "Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis",
+        "Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque"
+      ]  
+    },
+    {
+      id: "experience-3",
+      title: "Somethin ENgineer",
+      Location: "Chihuahua, Mexico",
+      company: "bar",
+      achievements: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque.", 
+        "Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis",
+        "Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque"
+      ]  
+    },
+  ];
+  
+export const projects = [
+    {
+      id: "project-1",
+      title: "Bicycle Maintenance Tracker",
+      tech: "ReactJS/Tailwind",
+      link: "3800+",
+    },
+    {
+      id: "project-2",
+      title: "Personal Webpage",
+      tech: "ReactJS/NextJS/Tailwind",
+      link: "230+",
+    },
+    {
+      id: "project-3",
+      title: "Algorithm Visualizer",
+      tech: "Javascript",
+      link: "$230M+",
+    },
+    {
+        id: "project-4",
+        title: "Travel Planner",
+        tech: "Javascript",
+        link: "$230M+",
+    },
+    {
+        id: "project-5",
+        title: "Automated Schedule Picker",
+        tech: "Python",
+        link: "$230M+",
+    },
+  ];
+  
+  export const media = [
+    {
+      id: "social-media-1",
+      icon: mail,
+      link: "https://www.gmail.com/",
+    },
+    {
+      id: "social-media-2",
+      icon: github,
+      link: "https://www.github.com/",
+    },
+    {
+      id: "social-media-3",
+      icon: linkedin,
+      link: "https://www.linkedin.com/",
+    },
+    {
+      id: "social-media-4",
+      icon: instagram,
+      link: "https://www.instagram.com/",
+    },
+  ];
