9c75089c2408642044fa029eda832ac5cff5fc86
Jay-DVT <diegovt.arbt@gmail.com>
2022-09-28 21:42:14 -0600

    add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>Diego Villanueva</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       </Head>
       <Header />
