b98a503de6247b69efcb1f8ef8d9aca6b5a4cebd
Jay-DVT <diegovt.arbt@gmail.com>
2022-10-04 10:28:56 -0600

    Structure section's header text

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -8,7 +8,7 @@ type Props = {}
 export default function Experience({}: Props) {
   return (
     <div className='h-screen flex relative flex-col max-w-7xl px-10 md:justify-center mx-auto items-center'>
-      <div className="relative flex flex-col py-5 items-center">
+      <div className="relative flex flex-col md:inset-y-20 py-5 items-center">
         <p className='md:text-[40px] text-[28px] font-bold'>Experience</p>
         <div className="flex-grow border-t border-white w-[80%]"></div>
       </div>
