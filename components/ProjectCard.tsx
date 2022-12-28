3d4fbddcbcfdd2c0f97cdb7afda3c6897005b4cc
Jay-DVT <diegovt.arbt@gmail.com>
2022-12-28 10:46:09 -0600

    Clean pop up selection bug

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -9,7 +9,7 @@ type Props = {}
 
 
 export default function ProjectCard({}: Props) {
-    const [selected, setSelected] = useState(0)
+    const [selected, setSelected] = useState(-1)
     
     const toggle = (i: number) => {
         setSelected(i)
@@ -30,7 +30,7 @@ export default function ProjectCard({}: Props) {
             <div key={index} className={`flex justify-center align-middle w-[270px] flex-col m-[2rem] border-[2px] hover:border-[#CAFC01] border-transparent p-[1rem] rounded-[0.5rem] bg-[#101010] cursor-pointer hover:shadow-lg`} >
                 <a href={project.link} onMouseEnter={() => toggle(index)} onMouseLeave={() => toggle(-1)}>
                     <div className='pt-0 mt-0 border-0 h-fill w-auto'>
-                        <Image key={project.title} src={project.image} alt={project.title}></Image>
+                        <Image key={project.title} src={project.image} alt={project.title} priority></Image>
                     </div>
                     <motion.div 
                     variants={variants}
